561d72fd01bac32c6e93432b262345dd5738a76d
Chufeng <huchufeng@g.ucla.edu>
2018-11-03 18:54:13 -0400

    add footer

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -5,6 +5,7 @@ import { Grid, Segment, Image } from 'semantic-ui-react';
 import Title from './components/title';
 import NavBar from './components/nav_bar';
 import SectionDetail from './components/section_detail';
+import Footer from './components/footer';
 
 class App extends Component {
 	constructor(props) {
@@ -34,6 +35,7 @@ class App extends Component {
 						<SectionDetail section={this.state.selectedSection} />
 					</Grid.Column>
 				</Grid>
+				<Footer />
 			</div>
 		);
 	}
