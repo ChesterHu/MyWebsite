0ab29c10a6b767da61e439cd20d814de68cb0b81
Chufeng <huchufeng@g.ucla.edu>
2018-11-02 22:31:52 -0400

    add title

---
+++ client/src/components/title.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Image } from 'semantic-ui-react';
+import { Container, Header, Segment } from 'semantic-ui-react';
+
+import NavBar from './nav_bar';
+
+const Title = () => {
+	return (
+		<Segment
+            inverted
+            textAlign='center'
+            style={{ minHeight: 700, padding: '1em 0em' }}
+            vertical
+          >
+			<Container text>
+				<Header 
+					as='h1'
+					content='Chester Hu'
+					inverted
+					style={{
+						fontSize: '4em',
+						fontWeight: 'normal',
+						marginBottom: 0,
+						marginTop: '3em'
+					}} />
+			</Container>
+		</Segment>
+	);
+}
+
+export default Title;
