9bf32eae77ec0b8b22ecab944908f4b87d564137
Chufeng <huchufeng@g.ucla.edu>
2018-11-02 22:31:34 -0400

    add nav bar

---
+++ client/src/components/nav_bar.jsx
@@ -0,0 +1,48 @@
+import React, { Component} from 'react';
+import { Container, Menu } from 'semantic-ui-react';
+
+class NavBar extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			sections: props.sections,
+			activeItem: 'Home'
+		};
+	}
+
+	render() {
+		const handleClick = (event, { name }) => { 
+			this.setState({ activeItem: name })
+		};
+		
+		const activeItem = this.state.activeItem;
+		const sectionItems = this.state.sections.map((sectionItem) => {
+			return (
+				<Menu.Item
+					name={sectionItem} 
+					active={activeItem === sectionItem}
+					onClick={handleClick}
+				/>
+			);
+		});
+
+
+		return (
+			<Menu 
+				fixed='top'
+				size='large'
+				pointing
+				secondary
+				inverted
+			>
+				<Container>
+					{sectionItems}
+				</Container>
+			</Menu>
+		);
+	}
+};
+
+
+export default NavBar;
