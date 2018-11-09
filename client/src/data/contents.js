6bc6d29c1a524173ba1acdc215efcd5a280ca919
Chufeng <huchufeng@g.ucla.edu>
2018-11-09 14:05:47 -0500

    init content.js

---
+++ client/src/data/contents.js
@@ -0,0 +1,18 @@
+const Contact = {
+	title: 'CONTACT',
+	subsections: [
+		{
+			header: 'If you want to discuss anything, please fell free to email me',
+			paragraphs: [
+				'Email: c55hu at uwaterloo.ca',
+				'Room 3594, Davis Centre',
+				'David Cheriton School of Computer Science',
+				'University of Waterloo, Ontario',
+				'L3R 8M6'
+			]
+		}
+	],
+};
+
+
+export { Contact };
