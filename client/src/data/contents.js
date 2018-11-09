dcf02dff90a6481830c2650725500fea275a5c77
Chufeng <huchufeng@g.ucla.edu>
2018-11-09 15:25:33 -0500

    add home object

--- client/src/data/contents.js
+++ client/src/data/contents.js
@@ -14,5 +14,19 @@ const Contact = {
 	],
 };
 
+const Home = {
+	title: 'HOME',
+	subsections: [
+		{
+			header: 'About me',
+			paragraphs: ["I'm a research-based Master student in computer science at University of Waterloo, I'm current working at the scientific computing lab under the supervision of Professor Yuying Li. My research focuses on reinforcement learning in finite horizon. Besides, I have a great interest in machine learning and software development."]
+		},
+
+		{
+			header: 'About this site',
+			paragraphs: ["Building my website with JavaScript is fulfilling and exciting, The difficult part for me was commencing the project and the motivation is to learn more about web application. This site is a React app written in ES6, the source code can be found in my [GitHub repository](https://github.com/ChesterHu/MyWebsite)."]
+		}
+	]
+};
 
 export { Contact };
