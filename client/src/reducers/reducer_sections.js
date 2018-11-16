8176d557fdc81b15d943e734c25ba26f596e133b
Chufeng <huchufeng@g.ucla.edu>
2018-11-16 15:24:00 -0500

    init sections reducer

---
+++ client/src/reducers/reducer_sections.js
@@ -0,0 +1,8 @@
+export default function() {
+	return [
+		{ title: 'Welcome to my website' },
+		{ title: 'Selected Projects' },
+		{ title: 'Teaching Experience' },
+		{ title: 'Contact Information' }
+	]
+}
