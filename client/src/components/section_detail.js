8f29b50326c73a3a3316207bc7d12acdbea872d0
Chufeng <huchufeng@g.ucla.edu>
2018-11-16 00:22:12 -0500

    change styles and section loading method

--- client/src/components/section_detail.js
+++ client/src/components/section_detail.js
@@ -1,29 +1,50 @@
 import React from 'react';
-import { Container } from 'semantic-ui-react';
+import { Container, Divider } from 'semantic-ui-react';
 import ReactMarkdown from 'react-markdown';
 
-import Home from '../data/home';
-import Contact from '../data/contact';
-import Projects from '../data/projects';
-import Teaching from '../data/teaching';
+import { home, projects, teaching, contact } from '../data/contents';
 
 const SectionDetail = ({ section }) => {
-	const content = getContent(section);
+	const selectedSection = getContent(section);
 	return (
 		<Container text>
-			<ReactMarkdown source={content} />
+			{renderSection(selectedSection)}
 		</Container>
 	);
 };
 
 function getContent(section) {
 	switch(section) {
-		case 'Home': return Home;
-		case 'Contact': return Contact;
-		case 'Projects': return Projects;
-		case 'Teaching': return Teaching;
+		case 'Home': return home;
+		case 'Contact': return contact;
+		case 'Projects': return projects;
+		case 'Teaching': return teaching;
 	}
 	return null;
+}
+
+function renderSection(section) {
+	const numSubsections = section.subsections.length;
+	const subsections = section.subsections.map((subsection, i)=>{
+		return (
+			<div>
+				<h3>{subsection.header && subsection.header}</h3>
+				{subsection.paragraphs.map((paragraph) => <p>{paragraph}</p> )}
+				{i < numSubsections - 1 && <Divider />}
+			</div>
+		);
+	});
+
+	return (
+		<div>
+			<header>
+				<h2>{section.title}</h2>
+				{section.subtitle && <p><i>{section.subtitle}</i></p>}
+			</header>
+			<Divider hidden />
+			{subsections}
+		</div>
+	);
 }
 
 export default SectionDetail;
