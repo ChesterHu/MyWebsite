7ecdc785677ab442573891ed35a5528bdb606eed
Chufeng <huchufeng@g.ucla.edu>
2018-11-16 19:12:21 -0500

    remove unused files

--- client/src/data/home.js
+++
@@ -1,9 +0,0 @@
-module.exports =  `
-# Welcome to my website!
-
-## About me
-I'm a research-based Master student in computer science at University of Waterloo, I'm current working at the scientific computing lab under the supervision of Professor Yuying Li. My research focuses on reinforcement learning in finite horizon. Besides, I have a great interest in machine learning and software development.
-
-## About this site
-Building my website with JavaScript is fulfilling and exciting, The difficult part for me was commencing the project and the motivation is to learn more about web application. This site is a React app written in ES6, the source code can be found in my [GitHub repository](https://github.com/ChesterHu/MyWebsite).
-`;
