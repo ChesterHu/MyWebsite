9f039c27394944a0adee3f9d8964ff2fd6e00cff
Chufeng <huchufeng@g.ucla.edu>
2018-11-22 13:55:43 -0500

    init config folder, init file to store game params

---
+++ client/src/config/gameParams.js
@@ -0,0 +1,7 @@
+export const NUM_ROWS = 10;
+export const NUM_COLS = 10;
+export const EMPTY = 'E';
+export const SHIP = 'S';
+export const HIT = 'H';
+export const MISS = 'M';
+export const ALL_SHIPS = [2, 3, 3, 5];
